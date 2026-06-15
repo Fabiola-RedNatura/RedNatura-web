e9a5d4ab8fac2e972922644ed81a5871d8873252
Fabiola-RedNatura <ashleymartinez1232@gmail.com>
2026-06-15 09:50:59 -0600

    Update sucursales.js

--- js/sucursales.js
+++ js/sucursales.js
@@ -1,1 +1,8 @@
+const sucursales = [
+  { ciudad: "Ciudad de México", estado: "CDMX" },
+  { ciudad: "Guadalajara", estado: "Jalisco" },
+  { ciudad: "Monterrey", estado: "Nuevo León" },
+  { ciudad: "Puebla", estado: "Puebla" },
+  { ciudad: "Toluca", estado: "Estado de México" }
+];
 
